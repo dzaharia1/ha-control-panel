276810679291062d667f3b21ddecc6c0840a268c
Dan Zaharia <zaharia.danny@gmail.com>
2024-03-16 00:24:27 -0400

    get buttons working

--- code.py
+++ code.py
@@ -17,12 +17,9 @@ buttons = [
     digitalio.DigitalInOut(board.D13),
     digitalio.DigitalInOut(board.D9)
 ]
-
 for button in buttons:
-    button.pull = digitalio.Pull.DOWN
     button.direction = digitalio.Direction.INPUT
-buttons[2].pull = digitalio.Pull.UP
-buttons[3].pull = digitalio.Pull.UP
+    button.pull = digitalio.Pull.DOWN
 
 lastEncoderPosition = None
 lastTemperaturePublish = time.monotonic()
@@ -47,19 +44,9 @@ def checkEncoder():
         lastEncoderPosition = newPosition
 
 def checkButtons():
-    if buttons[0].value == True:
-        print("Button {}".format(0))
-    if buttons[1].value == True:
-        print("Button {}".format(1))
-    if buttons[2].value == False:
-        print("Button {}".format(2))
-    if buttons[3].value == False:
-        print("Button {}".format(3))
-    if buttons[4].value == True:
-        print("Button {}".format(4))
-    # if buttons[5].value == True:
-    #     print("Button {}".format(5))
-    print(buttons[5].value)
+    for i in range(len(buttons)):
+        if buttons[i].value == True:
+            feeds.publish(feeds.commanderFeed, i + 1)
 
 def mqtt_message(client, feed_id, payload):
     print("Got {} from {}".format(payload, feed_id))
@@ -92,6 +79,6 @@ while (True):
         feeds.publish(feeds.temperatureSettingFeedCommand, display.temperatureSetting)
         lastTemperaturePublish = time.monotonic()
         newTempPublished = True
-    # checkButtons()
+    checkButtons()
     feeds.loop()
     time.sleep(.1)
